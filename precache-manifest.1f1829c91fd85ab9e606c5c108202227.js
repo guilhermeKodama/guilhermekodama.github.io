3f340c4c92edbb4e453edbf29881ececac092f5c
Guilherme Kodama <guilherme.kodama@gmail.com>
2019-04-29 19:02:15 -0300

    Updates

---
+++ precache-manifest.1f1829c91fd85ab9e606c5c108202227.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d563c12d913b52861847fb0e2b0a67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e6486e815946edf20e0",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "81c0a256b0af8d78ea00",
+    "url": "/static/js/2.598ad438.chunk.js"
+  },
+  {
+    "revision": "5e6486e815946edf20e0",
+    "url": "/static/js/main.b3ead4a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2abbb73baf64e4e01c7c17882f1d50d2",
+    "url": "/static/media/carrercon.2abbb73b.png"
+  },
+  {
+    "revision": "b475627069262c1f2af902326811802d",
+    "url": "/static/media/closetinn.b4756270.png"
+  },
+  {
+    "revision": "8216ba75dbb0642463164e97d93b50ff",
+    "url": "/static/media/me_square.8216ba75.png"
+  },
+  {
+    "revision": "0b5c2c2df35eaf0bc5ba48813a4089cd",
+    "url": "/static/media/pandora.0b5c2c2d.png"
+  },
+  {
+    "revision": "9981885ab281f0e1d5889dacf34c92e3",
+    "url": "/static/media/petfinder.9981885a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.380adf610922866d450d1a7155a60b54.js"
+  "/precache-manifest.1f1829c91fd85ab9e606c5c108202227.js"
 );
 
 self.addEventListener('message', (event) => {
